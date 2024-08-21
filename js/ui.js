418571afe6e54917466a8ba5cf17c2ca2c38294c
Bonex1s <bonex1@ukr.net>
2024-08-21 05:07:24 +0300

    faatback and cards

--- js/ui.js
+++ js/ui.js
@@ -34,4 +34,47 @@ document.addEventListener("DOMContentLoaded", function () {
       header.classList.remove("scrolled");
     }
   });
+  document
+    .querySelector(".carousel-container")
+    .addEventListener("click", function () {
+      const prev = document.querySelector(".prev-block");
+      const active = document.querySelector(".active-block");
+      const next = document.querySelector(".next-block");
+
+      prev.classList.remove("prev-block");
+      prev.classList.add("active-block");
+
+      active.classList.remove("active-block");
+      active.classList.add("next-block");
+
+      next.classList.remove("next-block");
+      next.classList.add("prev-block");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const leftArrow = document.getElementById("left-arrow");
+  const rightArrow = document.getElementById("right-arrow");
+  const pages = document.querySelectorAll(".card-list");
+  let currentPage = 0;
+
+  function showPage(index) {
+    pages.forEach((page, i) => {
+      page.style.display = i === index ? "grid" : "none";
+      console.log("ahahahah");
+    });
+  }
+
+  leftArrow.addEventListener("click", () => {
+    currentPage = currentPage > 0 ? currentPage - 1 : pages.length - 1;
+    showPage(currentPage);
+  });
+
+  rightArrow.addEventListener("click", () => {
+    currentPage = currentPage < pages.length - 1 ? currentPage + 1 : 0;
+    showPage(currentPage);
+  });
+
+  // Изначально показываем первую страницу
+  showPage(currentPage);
 });
